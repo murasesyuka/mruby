aa19cad2ae915425289b0faae2349ac64d4e8907
murase_syuka <murase.syuka+github@gmail.com>
2013-04-13 10:31:10 +0900

    Add Integer#udiv_round to numeric.rb
    ex.
      1.udiv(2)       => 0
      1.udiv_round(2) => 1

--- mrblib/numeric.rb
+++ mrblib/numeric.rb
@@ -89,6 +89,11 @@ class Integer
     end
     self
   end
+
+  def udiv_round(num)
+    a, b = self, num.to_i
+    (a + b.udiv(2)).udiv(b)
+  end
 end
 
 ##
--- test/t/numeric.rb
+++ test/t/numeric.rb
@@ -30,3 +30,9 @@ end
 assert('Integer#udiv') do
   4.udiv 3 == 1
 end
+
+assert('Integer#udiv_round') do
+  192.udiv_round(127) == 2
+  192.udiv_round(128) == 2
+  192.udiv_round(129) == 1
+end
